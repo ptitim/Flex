488f848c5edd4e53a0fdffe16c7d291d0a678839
ptitim <timgamer@hotmail.fr>
2016-08-31 16:44:20 +0200

    correction de la position de la navbar lors du scroll et ajout du scroll auto

--- script.js
+++ script.js
@@ -2,25 +2,28 @@
 // ----- script navbar ----- //
 var header = document.querySelector('header');
 var headerContent = document.getElementById('header');
-var firstArticle = document.querySelectorAll('article')[0];
+var image = document.getElementById('img');
+// var firstArticle = document.querySelectorAll('article')[0];
 
 var scrollY = function() {
     var supportPageOffset = window.pageXOffset !== undefined;
     var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
     return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
 };
+
 var onScroll = function() {
     if (scrollY() > 40) {
+        img.style.marginTop = "15vh";
         header.style.position = "fixed";
-        header.style.height = "15%";
-        headerContent.style.height = "10%";
-        firstArticle.style.marginTop = "220px";
+        header.style.height = "11vh";
+        headerContent.style.height = "10vh";
+        // firstArticle.style.marginTop = "220px";
 
     } else {
         header.style.position = "relative";
         header.style.height = "110px";
         headerContent.style.height = "110px";
-        firstArticle.style.marginTop = "0px";
+        // firstArticle.style.marginTop = "0px";
     }
 };
 window.addEventListener('scroll', onScroll);
--- scriptJq.js
+++ scriptJq.js
@@ -5,25 +5,29 @@ $(document).ready(function(){
           // event.preventDefault();
           var hash = this.hash;
 
+          console.log(hash, hashp);
           if(hashp == 0){
             var hauteurActu = 0;
           }else {
             var hauteurActu = $(hashp).offset().top;
           }
           var hauteurScroll = $(hash).offset().top;
+          console.log(hauteurActu,", ",hauteurScroll);
           if(hauteurActu < hauteurScroll){
+
               $('html, body').animate({
-                  scrollTop: ($(hash).offset().top)+100
+                  scrollTop: ($(hash).offset().top)+25
               },"slow");
           }else{
+
               $('html, body').animate({
-                  scrollTop: ($(hash).offset().top)-100
+                  scrollTop: ($(hash).offset().top)-125
             },"slow");
           };
           $('html, body').animate({
-              scrollTop: ($(hash).offset().top)
-          },100,function(){
-            window.location.hash = hash;
+              scrollTop: ($(hash).offset().top)-100
+          },'slow',function(){
+            // window.location.hash = hash;
             hashp = hash;
           })
         }

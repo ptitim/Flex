65d3a0632a26ff18018629a23e76b04872e618a9
ptitim <timgamer@hotmail.fr>
2016-09-05 16:54:05 +0200

    ajout du premier exemple

---
+++ exemple.js
@@ -0,0 +1,33 @@
+// Cahque fonction sera pour un exemple, exepter le FlexDir qui s'apliquera sur l'id envoyer en parametre
+function flexDir(id, id2){
+    var ele = document.getElementById(id);//recuperation de l'element a changez d'attribut css
+    var afgAttribut = document.getElementById(id2);//element html pour afficher l'etat de l'attribut css
+    var flexDirTab = ["row", "row-reverse", "column", "column-reverse"];//attribut du style flex
+    var styleAct = ele.style.flexDirection;//recuperation du style actuel
+
+    var index = flexDirTab.indexOf(styleAct);
+    index++;
+    index >= flexDirTab.length ? index = 0 : index = index;//condition pour ne pas depacer du tableau
+    ele.style.flexDirection = flexDirTab[index];
+    afgAttribut.innerText  = flexDirTab[index];
+}
+
+function wraper(){
+    var ele = document.getElementById('exempleDistrib');
+    var texte = document.getElementById('wrap');
+    var wrapAtr = ['wrap', 'wrap-reverse', 'nowrap'];
+    var actu = ele.style.flexWrap;
+
+    if(actu == ""){
+        ele.style.flexWrap = "wrap";
+    }
+    var truc = wrapAtr.indexOf(actu);//index
+    truc++;
+    truc >= wrapAtr.length ? truc = 0 : truc = truc;
+    ele.style.flexWrap = wrapAtr[truc];
+    texte.innerText = wrapAtr[truc];
+}
+function init(){
+  flexDir('exempleDistrib','flexDir1');
+  wraper();
+}
--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 var header = document.querySelector('header');
 var headerContent = document.getElementById('header');
 var image = document.getElementById('img');
-// var firstArticle = document.querySelectorAll('article')[0];
 
 var scrollY = function() {
     var supportPageOffset = window.pageXOffset !== undefined;
@@ -13,18 +12,17 @@ var scrollY = function() {
 
 var onScroll = function() {
     if (scrollY() > 40) {
-        img.style.marginTop = "15vh";
+        image.style.marginTop = "15vh";
         header.style.position = "fixed";
         header.style.height = "11vh";
         headerContent.style.height = "10vh";
-        // firstArticle.style.marginTop = "220px";
 
     } else {
-      img.style.marginTop = "0";
+      image.style.marginTop = "0";
         header.style.position = "relative";
         header.style.height = "110px";
         headerContent.style.height = "110px";
-        // firstArticle.style.marginTop = "0px";
     }
 };
 window.addEventListener('scroll', onScroll);
+onScroll();

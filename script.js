8b4bf18e5c8f411b0f7677cf9381181ca6fb5513
BlandineRolland <blandine.rolland@gmail.com>
2016-09-21 15:24:43 +0200

    latest version

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-
-// ----- script navbar ----- //
-var header = document.querySelector('header');
-var headerContent = document.getElementById('header');
-var image = document.getElementById('img');
-
-var scrollY = function() {
-    var supportPageOffset = window.pageXOffset !== undefined;
-    var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-    return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
-};
-
-var onScroll = function() {
-    if (scrollY() > 40) {
-        image.style.marginTop = "110px";
-        header.style.position = "fixed";
-        header.style.height = "70px";
-        headerContent.style.height = "70px";
-
-    } else {
-      image.style.marginTop = "0";
-        header.style.position = "relative";
-        header.style.height = "110px";
-        headerContent.style.height = "110px";
-    }
-};
-window.addEventListener('scroll', onScroll);
-onScroll();
-=======
 
 // ----- script navbar ----- //
 var header = document.querySelector('header');
@@ -56,4 +26,3 @@ var onScroll = function() {
 };
 window.addEventListener('scroll', onScroll);
 onScroll();
->>>>>>> Simplon-BlandineR-master
